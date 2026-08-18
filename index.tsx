6f342414a6668383cdae6d9476adc82dc1a38462
Adib23704 <adib23704@gmail.com>
2026-08-19 02:52:19 +0600

    update

--- index.tsx
+++ index.tsx
@@ -31,7 +31,7 @@ const fakeApplications = new Map();
 export default definePlugin({
     name: "CompleteDiscordQuest",
     description: "A plugin that completes multiple discord quests in background simultaneously.",
-    authors: [Devs.nicola02nb, Devs.syntt, Devs.djdoolky76],
+    authors: [{ name: "__azuree__", id: 451657007791996929n }],
     settings,
     patches: [
         {
